02b56d62c928ccbe01f21a00d566271543b05e28
Коробкова Мария Николаевна <Korobkova_MN@cd.local>
2020-10-17 21:51:52 +0300

    added some media queries

--- js/controls.js
+++ js/controls.js
@@ -6,6 +6,14 @@ button.addEventListener("click", function () {
 	video.setAttribute("controls", "controls");
 }, false);
 
+var videoMobile = document.querySelector("#video-mobile"),
+	button = document.querySelector("#button");
+
+button.addEventListener("click", function () {
+	videoMobile.play()
+	videoMobile.setAttribute("controls", "controls");
+}, false);
+
 $(".presentation__btn").click(function () {
 	$(".presentation-box").addClass('presentation-box-hide');
 })
